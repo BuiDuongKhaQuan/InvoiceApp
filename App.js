13883fa0ca330880cf362d26d740337453367f6b
BuiDuongKhaQuan <113542708+BuiDuongKhaQuan@users.noreply.github.com>
2023-09-16 14:37:02 +0700

    Update App.js

--- App.js
+++ App.js
@@ -7,23 +7,13 @@ import ChangePassword from './screens/ChangePassword';
 import TabNavigator from './screens/TabNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-const Stack = createNativeStackNavigator();
-
 import InvoiceItem from './components/InvoiceItem';
 import InvoiceList from './screens/InvoiceList';
 import Setting from './screens/Setting';
 import Support from './screens/Support';
-
 import Profile from './screens/Profile';
-export default function App() {
-    const headerNone = { headerShown: false };
-    return (
-        <Profile />
 
-import Language from './screens/Language';
-import Home from './screens/Home';
-import QR from './screens/ScanQR';
+const Stack = createNativeStackNavigator();
 export default function App() {
     const headerNone = { headerShown: false };
     return (
