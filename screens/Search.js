82554b0ecf342020c64e1121903485e975c390d4
PhanThiHongSiem <20130386@st.hcmuaf.edu.vn>
2023-09-25 15:08:38 +0700

    find id by invoice

--- screens/Search.js
+++ screens/Search.js
@@ -1,13 +1,54 @@
-import { StatusBar, StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StatusBar, StyleSheet, Text, View, Dimensions, Alert } from 'react-native';
 import { white } from '../constant/color';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Input from '../components/Input';
 import Button from '../components/Button';
 import { fontSizeDefault } from '../constant/fontSize';
 import { Feather, AntDesign, Ionicons } from '@expo/vector-icons';
 import BackgroundImage from '../layouts/DefaultLayout/BackgroundImage';
-
+import { getIdInvoice } from '../Service/api';
+import axios from 'axios';
 export default function Search({ navigation }) {
+    const [id, setId] = useState('');
+    const [invoice, setInvoice] = useState(null);
+    const [error, setError] = useState(null);
+
+    const handleSearch = async () => {
+        try {
+            const response = await axios.get(
+                `http://bill-rest.ap-southeast-2.elasticbeanstalk.com/api/v1/invoices/id/${id}`,
+            );
+            const data = response.data;
+
+            if (data) {
+                // Cập nhật thông tin hóa đơn
+                setInvoice(data);
+                setError(null); // Xóa thông báo lỗi nếu có
+
+                // Hiển thị thông báo thông tin hóa đơn
+                Alert.alert(
+                    'Thông tin hóa đơn',
+                    `ID: ${data.id}\nStatus: ${data.status}\nNgày tạo: ${data.createdAt}\nUpdated_at: ${data.updatedAt}\ Is_paid: ${data.is_paid}\nNote: ${data.note}\nCompany_id: ${data.company_id}\nMethod: ${data.method}\nUser_id: ${data.user_id}`,
+                );
+            } else {
+                // Hiển thị thông báo nếu ID không khớp
+                Alert.alert('Lỗi', 'Không tìm thấy hóa đơn với ID này');
+                setError('Không tìm thấy hóa đơn với ID này');
+                setInvoice(null); // Đặt hóa đơn thành null khi có lỗi
+            }
+        } catch (error) {
+            console.error('Lỗi khi gọi API:', error);
+            // Xử lý lỗi và cập nhật thông báo lỗi
+            setError('Không tìm thấy hóa đơn với ID này');
+            setInvoice(null); // Đặt hóa đơn thành null khi có lỗi
+            // Hiển thị thông báo lỗi
+            // if (error.response) {
+            //     console.error('Chi tiết lỗi từ phản hồi:', error.response.data);
+            // }
+            Alert.alert('Lỗi', 'Không tìm thấy hóa đơn với ID này');
+        }
+    };
+
     return (
         <View style={styles.container}>
             <BackgroundImage>
@@ -18,8 +59,12 @@ export default function Search({ navigation }) {
                         iconLeft={<Feather name="search" size={24} color="black" />}
                         iconRight={<Ionicons name="ios-qr-code-outline" size={24} color="black" />}
                         onPressIconRight={() => navigation.navigate('Scanner')}
+                        value={id}
+                        onChangeText={(text) => setId(text)}
+                        onSubmitEditing={handleSearch}
                     />
                 </View>
+
                 <View style={styles.container_top}>
                     <View style={styles.result}>
                         <Button
